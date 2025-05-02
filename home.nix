bb9a0a42d665d138585c2f1bd203232bf600045e
ZemyDios <tonicrespomengual@gmail.com>
2025-05-02 02:24:28 +0200

    Set up firefox.nix as module

--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   imports = [
     ./modules/shells.nix
     ./modules/hyprland.nix
+    ./modules/firefox.nix
   ];
 
   home.username = "zemy";
@@ -34,14 +35,4 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.firefox = {
-    enable = true;
-    profiles.zemy = {
-      isDefault = true;
-      settings = {
-        "browser.startup.homepage" = "https://google.com/";
-        "browser.startup.page" = 3; # 0 = about:blank, 1 = home page, 2 = last visited page, 3 = resume previous session
-      };
-    };
-  };
 }
