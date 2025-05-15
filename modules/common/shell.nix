848fe6d5165e9e2c4e81f699ae8a1459bb85e1d0
ZemyDios <tonicrespomengual@gmail.com>
2025-05-16 00:49:09 +0200

    Add oh-my-zsh and p10k

--- modules/common/shell.nix
+++ modules/common/shell.nix
@@ -5,5 +5,10 @@
   environment.shells = with pkgs; [bash zsh]; # System Shells
   users.defaultUserShell = pkgs.zsh; # Default shell
 
+  environment.systemPackages = with pkgs; [
+    oh-my-zsh
+    zsh-powerlevel10k
+  ];
+
   programs.zsh.enable = true;
 }
